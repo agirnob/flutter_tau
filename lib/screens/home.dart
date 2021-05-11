c802bef57d20f75fbb734c160204cdaa91c580b3
agirnob <kakcayigitbaran@gmail.com>
2021-05-11 17:26:34 +0300

    some testing about flutter and scenes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,12 +2,14 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import '../main.dart';
+import '../Util.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     MyApp color = new MyApp();
+    //TODO buradaki column u layout ile düzeltmek
     return Container(
       child: Column(
         children: [
@@ -15,30 +17,43 @@ class Home extends StatelessWidget {
             alignment: Alignment.topLeft,
             child: Text(
               "Duyurular",
+              style: TextStyle(color: Colors.white, fontSize: 35),
               textDirection: TextDirection.ltr,
               textAlign: TextAlign.end,
-
+            ),
+          ),
+          //Ana sayfadaki yazı ve takvimin gözükmesi için
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Stack(
+              children: <Widget>[
+                new Image.asset(
+                  'images/calendar-blank-line.png',
+                  width: 100,
+                  height: 100,
+                ),
+                Text(
+                  sayHello(),
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ],
             ),
           )
         ],
       ),
       alignment: Alignment.topCenter,
-      color: color.createMaterialColor(Color(0xFF25B0D0)),
+      color: createMaterialColor(Color(0xFF25B0D0)),
       width: MediaQuery.of(context).size.width,
-      height: 96.0,
+      height: 240.0,
     );
   }
 
   String sayHello() {
     String sayThis;
-    DateTime now = new DateTime.now();
-    int saat = now.hour;
-    if (saat <= 12) {
-      sayThis = "GUten Morgen herr son of biscuit";
-    } else if (saat <= 18) {
-      sayThis = "GUten Tag herr son of biscuit";
+    if (DateTime.now().day < 10) {
+      sayThis = "0" + DateTime.now().day.toString();
     } else {
-      sayThis = "go to  fucking sleep herr son of biscuit";
+      sayThis = DateTime.now().day.toString();
     }
     return sayThis;
   }
